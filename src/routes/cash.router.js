ddf91cc7ab84872c0c491cb6344afb6a3de27fe4
Eliot Jang <eliotjang2@gmail.com>
2024-06-01 01:37:46 +0900

    update - authMiddleware 인증

--- src/routes/cash.router.js
+++ src/routes/cash.router.js
@@ -1,15 +1,11 @@
 import express from 'express';
-//import authMiddleware from '../middlewares/auth.middleware.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 import { prisma } from '../utils/prisma/index.js';
 
 const router = express.Router();
 
 // 캐시 구매 API (JWT 인증)
-/*
-  authMiddleware 인증 추가되면 파라미터에 넣어 반영 예정
-  ex) router.get('/cash', authMiddleware, async (req, res, next) => {});
-*/
-router.patch('/cash', async (req, res, next) => {
+router.patch('/cash', authMiddleware, async (req, res, next) => {
   try {
     const { userId } = req.user;
     await prisma.character.update({
