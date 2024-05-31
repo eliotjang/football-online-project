317a9c508d3037df21c031c771d9e36e3ddc975a
nalyn87 <nalyn0807@gmail.com>
2024-06-01 00:16:46 +0900

    update - 로그인 API 구현 완료

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,6 +3,8 @@ import Joi from 'joi';
 import { prisma } from '../utils/prisma/index.js';
 import bcrypt from 'bcrypt';
 import { Prisma } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import configs from '../utils/configs.js';
 
 const router = express.Router();
 
@@ -71,6 +73,38 @@ router.post('/sign-up', async (req, res, next) => {
     );
 
     return res.status(201).json({ message: '회원가입이 완료되었습니다.', userData: user, characterData: character });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 로그인 API
+router.post('/sign-in', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 유저 존재 유무 확인
+    const user = await prisma.user.findFirst({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(404).json({ errorMessage: '존재하지 않는 이메일입니다.' });
+    }
+
+    // 비밀번호 확인
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user.userId,
+      },
+      configs.tokenSecretKey
+    );
+
+    res.cookie('authorization', `Bearer ${token}`);
+    return res.status(200).json({ message: '로그인에 성공했습니다.' });
   } catch (err) {
     next(err);
   }
