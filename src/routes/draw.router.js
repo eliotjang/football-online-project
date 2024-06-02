26c1eb18ffe32f53318104db552188f3266ef0a2
injune200 <112794665+injune200@users.noreply.github.com>
2024-06-02 20:30:23 +0900

    update-희귀도에 따른 선수 뽑기 API 기능 구현 완료

--- src/routes/draw.router.js
+++ src/routes/draw.router.js
@@ -20,10 +20,35 @@ router.post('/draw', authMiddleware, async (req, res, next) => {
       return res.status(400).json({ Message: '보유 캐쉬가 부족합니다.' });
     }
 
+    const tier0 = 0.05; //티어에 따른 확률 
+    const tier1 = 0.1;
+    const tier2 = 0.15;
+    const tier3 = 0.2;
+    const tier4 = 0.2;
+    const tier5 = 0.3;
+
+    let rarity = 0;
+
+    const randomNumRarity = Math.random(); //희귀 등급 뽑기
+    if(randomNumRarity < tier0){
+      rarity = 0;
+    }else if(randomNumRarity < tier0 + tier1){
+      rarity = 1;
+    }else if(randomNumRarity < tier0 + tier1 + tier2 ){
+      rarity = 2;
+    }else if(randomNumRarity < tier0 + tier1 + tier2 + tier3 ){
+      rarity = 3;
+    }else if(randomNumRarity < tier0 + tier1 + tier2 + tier3 + tier4){
+      rarity = 4;
+    }else if(randomNumRarity < tier0 + tier1 + tier2 + tier3 + tier4 + tier5){
+      rarity = 5;
+    }
+
     const playerList = await prisma.player.findMany({
       //뽑기 선수 리스트 조회
       where: {
-        upgradeLevel: 0,
+        rarity: rarity,
+        upgradeLevel: 0
       },
     });
 
