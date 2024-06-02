e6a0c94a457c17998d352d1ae6392b60be065498
ydh1503 <ydh1503@naver.com>
2024-06-02 19:04:45 +0900

    update - upgrade 기능 구현 라우터 추가

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import RosterRouter from '../src/routes/roster.router.js';
 import RankingSystemRouter from '../src/routes/ranking-system.router.js';
 import DrawRouter from './routes/draw.router.js';
 import GameRouter from './routes/game.router.js';
+import UpgradeRouter from './routes/upgrade.router.js';
 import errorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import config from './utils/configs.js';
 import cookieParser from 'cookie-parser';
@@ -19,7 +20,16 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [UserRouter, CharacterRouter, CashRouter, RosterRouter, DrawRouter, GameRouter, RankingSystemRouter]);
+app.use('/api', [
+  UserRouter,
+  CharacterRouter,
+  CashRouter,
+  RosterRouter,
+  DrawRouter,
+  GameRouter,
+  RankingSystemRouter,
+  UpgradeRouter,
+]);
 app.use(errorHandlingMiddleware);
 
 app.listen(PORT, () => {
