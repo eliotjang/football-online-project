31b63ad6c06c7f6bb773d09d6a622a43fcf2ff48
nalyn87 <nalyn0807@gmail.com>
2024-06-02 18:20:05 +0900

    update - 보유 선수 목록 조회 API 구현 완료

---
+++ src/routes/player.router.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+// 보유 선수 목록 조회 API
+router.get('/players', authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+
+    const character = await prisma.character.findFirst({
+      where: {
+        UserId: +userId,
+      },
+    });
+
+    const characterPlayers = await prisma.characterPlayer.findMany({
+      where: {
+        CharacterId: character.characterId,
+      },
+    });
+
+    const characterPlayersData = [];
+    for (const p of characterPlayers) {
+        const player = await prisma.player.findFirst({
+            where: {
+                playerId: p.playerId,
+                upgradeLevel: p.upgradeLevel,
+            }
+        })
+
+        characterPlayersData.push(player);
+    }
+
+    return res.status(200).json({ message: '현재 캐릭터가 보유한 선수 목록입니다.', characterPlayersData});
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
